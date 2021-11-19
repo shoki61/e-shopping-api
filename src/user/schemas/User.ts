eca0f077e153bccad945fef43265b48021e0e665
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-11-19 23:13:48 +0300

    create user schema and connect DB

---
+++ src/user/schemas/User.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ required: true, type: String })
+  email: string;
+
+  @Prop({ required: true, type: String, minlength: 8 })
+  password: string;
+
+  @Prop({ required: true, type: Boolean, default: false })
+  emailVerified: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
