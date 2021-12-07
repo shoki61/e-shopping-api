cceee8b4c0df24d43ed62ee81de6643d6aec0c92
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-12-07 11:25:40 +0300

    set post variable

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3080;
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3080);
+  await app.listen(port);
 }
 bootstrap();
