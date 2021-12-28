99472a9e653561c39be01c11ca6634e669c44ae0
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-12-28 19:08:43 +0300

    fix and create product schema

---
+++ src/product/schemas/Product.ts
@@ -0,0 +1,40 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+import { Categories, MainCategories, SubCategories } from '../constants';
+
+export type ProductDocument = Product & Document;
+
+@Schema({ collection: 'products', timestamps: true })
+export class Product {
+  @Prop({ required: true, type: String })
+  name: string;
+
+  @Prop({ type: String })
+  description: string;
+
+  @Prop({ required: true, type: Number })
+  price: number;
+
+  @Prop({ type: Array })
+  colors: string[];
+
+  @Prop({ enum: MainCategories })
+  mainCategory: MainCategories;
+
+  @Prop({ required: true, enum: Categories })
+  category: Categories;
+
+  @Prop({ required: true, enum: SubCategories })
+  subCategory: SubCategories;
+
+  @Prop({ type: Number })
+  rating: number;
+
+  @Prop({ type: Number })
+  ratingCount: number;
+
+  @Prop({ required: true, type: String })
+  seller: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
--- src/user/dto/UserDto.ts
+++ src/user/dto/UserDto.ts
@@ -1,9 +1,15 @@
+import { IsBoolean, IsEmail, IsString } from 'class-validator';
+
 class User {
+  @IsString()
   name: string;
 
+  @IsEmail()
   email: string;
 
+  @IsString()
   password: string;
 
+  @IsBoolean()
   emailVerified: boolean;
 }
