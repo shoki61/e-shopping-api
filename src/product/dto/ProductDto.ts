41f60b34e093ae6d8b65b30800306f09a76dae65
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-12-28 19:07:33 +0300

    create product dto

---
+++ src/product/dto/ProductDto.ts
@@ -0,0 +1,34 @@
+import { IsArray, IsEnum, IsNumber, IsString } from 'class-validator';
+import { Categories, MainCategories, SubCategories } from '../constants';
+
+export class ProductDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  description: string;
+
+  @IsNumber()
+  price: number;
+
+  @IsArray()
+  colors: string[];
+
+  @IsEnum(MainCategories)
+  mainCategory: MainCategories;
+
+  @IsEnum(Categories)
+  category: Categories;
+
+  @IsEnum(SubCategories)
+  subCategory: SubCategories;
+
+  @IsNumber()
+  rating: number;
+
+  @IsNumber()
+  ratingCount: number;
+
+  @IsString()
+  seller: string;
+}
